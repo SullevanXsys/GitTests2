d2c05cbf4d64572114e28365a973c002c3c5dc5a
João Rodrigo <rodrigo@xsys.com.br>
2023-03-16 14:00:57 -0300

    FeatureD - Acrescentamos um Summarie FeatureD

--- GitTests/Controllers/FeatureBController.cs
+++ GitTests/Controllers/FeatureBController.cs
@@ -8,8 +8,8 @@ namespace GitTests.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "FeatureD"
+        };
 
         private readonly ILogger<FeatureBController> _logger;
 
