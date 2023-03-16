97d236a2dba3383e96fee923bded18b2f56164d0
João Rodrigo <rodrigo@xsys.com.br>
2023-03-16 13:44:25 -0300

    FeatureA

---
+++ GitTests/Controllers/FeatureAController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitTests.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FeatureAController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<FeatureAController> _logger;
+
+        public FeatureAController(ILogger<FeatureAController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecastA")]
+        public IEnumerable<WeatherForecast> GetFeatureA()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
